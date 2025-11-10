b297e6edb922a83a584cec882f3335e608d77767
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-11 00:06:29 +0200

    nu

--- CalcPi copy.java
+++ CalcPi copy.java
@@ -3,22 +3,21 @@ public class CalcPi {
 		int terms = Integer.parseInt(args[0]);
 		double javaPi = Math.PI;
 		double piApprox = 0.0;
-		for (int i = 1; i <= terms *2 -1 ; i += 4){
-			System.out.printf("+%d\n", i);
+		for (int i = 1; i <= terms *2 ; i += 4){
 			//System.out.println(i);
 			//System.out.println(1.0/i);
 			piApprox += 1.0/i;
 
 		}
-		for (int j = 3; j <= terms*2 -1; j += 4){
-			System.out.println(j);
+		for (int j = 3; j <= terms*2 ; j += 4){
+			//System.out.println(j);
 			//System.out.println(1.0/j);
 			piApprox -= 1.0/j;
 		}
 		
-		piApprox = piApprox * 4.0;
+		piApprox = piApprox *4.0;
 		System.out.printf("pi according to Java: %.15f\n", javaPi );
-		System.out.printf("pi, approximated:	%.15f", piApprox);
+		System.out.printf("pi, approximated:	 %.15f", piApprox);
 
 	}
 }
