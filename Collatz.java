e81e422760f25d6dd0171cabcedf820d148f2aa9
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-11 23:36:13 +0200

    3rd try collatz

--- Collatz.java
+++ Collatz.java
@@ -23,11 +23,11 @@ public class Collatz {
 					}
 				}
 				System.out.println (num + " (" + count + ")");
-				System.out.printf("Every one of the first %d hailstone sequences reached 1.", seed );
 				//System.out.println(count);
 				num = i;
 				count = 0;
 			}
+			System.out.printf("Every one of the first %d hailstone sequences reached 1.", seed );
 		}
 
 	    // Replace this comment with your code
