539e0aa3fbdbfc7513ab9df9f962c7b1489a258d
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-11 17:45:21 +0200

    no fstring

--- CalcPi.java
+++ CalcPi.java
@@ -3,22 +3,30 @@ public class CalcPi {
 		int terms = Integer.parseInt(args[0]);
 		double javaPi = Math.PI;
 		double piApprox = 0.0;
-		for (int i = 1; i <= terms *2 -1 ; i += 4){
-			//System.out.printf("+%d\n", i);
-			//System.out.println(i);
-			//System.out.println(1.0/i);
-			piApprox += 1.0/i;
 
+		for (int i = 0; i < terms; i++){
+			if (i % 2 == 0){
+				piApprox += 1.0 / (2 * i + 1);
+			} else {
+				piApprox -= 1.0 / (2 * i + 1);
+			}
 		}
-		for (int j = 3; j <= terms*2 -1; j += 4){
-			//System.out.println(j);
-			//System.out.println(1.0/j);
-			piApprox -= 1.0/j;
-		}
+		// for (int i = 1; i <= terms *2 -1 ; i += 4){
+		// 	//System.out.printf("+%d\n", i);
+		// 	//System.out.println(i);
+		// 	//System.out.println(1.0/i);
+		// 	piApprox += 1.0/i;
+
+		// }
+		// for (int j = 3; j <= terms*2 -1; j += 4){
+		// 	//System.out.println(j);
+		// 	//System.out.println(1.0/j);
+		// 	piApprox -= 1.0/j;
+		// }
 		
 		piApprox = piApprox * 4.0;
-		System.out.printf("pi according to Java: %.15f\n", javaPi );
-		System.out.printf("pi, approximated:	%.15f", piApprox);
+		System.out.println("pi according to Java: " + javaPi);
+		System.out.println("pi, approximated:	 " + piApprox);
 
 	}
 }
